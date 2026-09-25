6467924aed5c2b5b8d925acc173ec531a43063a7
Sudheer Kumar Das <samratsudheerdas@gmail.com>
2026-09-26 04:46:52 +0545

    routes added for getting content and deleting content

--- backend/src/routes/contentRoutes.js
+++ backend/src/routes/contentRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 
 import authMiddleware from "../middlewares/userMiddlewares.js";
-import { contentCreate, getAllContents } from "../controllers/contentControllers.js";
+import { contentCreate, deleteAContent, getAContent, getAllContents } from "../controllers/contentControllers.js";
 
 const router = express();
 
 router.post("/create",authMiddleware,contentCreate);
 router.get("/get-contents",authMiddleware,getAllContents);
+router.get("/get-content",authMiddleware,getAContent);
+router.delete("/delete-content",authMiddleware,deleteAContent);
 
 
 export default router;
