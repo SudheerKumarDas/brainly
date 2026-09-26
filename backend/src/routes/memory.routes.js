1a8ff4f08e9981a0dc89de5e4dafce1b30823fb8
Sudheer Kumar Das <samratsudheerdas@gmail.com>
2026-09-26 13:35:28 +0545

    memory is archived or not done

--- backend/src/routes/memory.routes.js
+++ backend/src/routes/memory.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import authMiddleware from "../middlewares/user.middlewares.js";
-import { createMemory, deleteMemory, getMemory, getAllMemories, updateMemory } from "../controllers/memory.controllers.js";
+import { createMemory, deleteMemory, getMemory, getAllMemories, updateMemory, toogleFavorite } from "../controllers/memory.controllers.js";
 
 const router = express();
 
@@ -10,6 +10,7 @@ router.get("/",authMiddleware,getAllMemories);
 router.get("/:id",authMiddleware,getMemory);
 router.patch("/:id",authMiddleware,updateMemory);
 router.delete("/:id",authMiddleware,deleteMemory);
+router.patch("/:id/fovorite",authMiddleware,toogleFavorite);
 
 
 export default router;
